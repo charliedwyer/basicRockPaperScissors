d34c52edf73a18988b4283a71f5617fe559a6f8d
Charlie Dwyer <cdwr17@gmail.com>
2023-06-20 17:05:24 -0500

    Add function to play round

--- javascript.js
+++ javascript.js
@@ -16,10 +16,26 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
+    console.log("You played " + playerSelection);
+    console.log("Computer played " + computerSelection);
+
+    if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || 
+    (playerSelection == "scissors" && computerSelection == "rock")) {
+        return "Ah-oh, " + computerSelection + " beats " + playerSelection + ", the computer wins!";
+    } else if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || 
+    (playerSelection == "rock" && computerSelection == "scissors")) {
+        return "YAY, " + playerSelection + " beats " + computerSelection + ", you win!";
+    } else if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    }
+}
 
 let playerSelection = prompt("Select Rock, Paper, or Scissors");
 let computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
 
 
