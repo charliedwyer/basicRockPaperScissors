03c869eb6a11f5e7d02e582f2eabd09791fa61d8
Charlie Dwyer <cdwr17@gmail.com>
2023-06-20 20:13:20 -0500

    Add function to play a game to 5

--- javascript.js
+++ javascript.js
@@ -24,18 +24,60 @@ function playRound (playerSelection, computerSelection) {
 
     if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || 
     (playerSelection == "scissors" && computerSelection == "rock")) {
+        computerScore++;
         return "Ah-oh, " + computerSelection + " beats " + playerSelection + ", the computer wins!";
     } else if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || 
     (playerSelection == "rock" && computerSelection == "scissors")) {
+        playerScore++;
         return "YAY, " + playerSelection + " beats " + computerSelection + ", you win!";
     } else if (playerSelection == computerSelection) {
         return "It's a tie!";
     }
 }
 
-let playerSelection = prompt("Select Rock, Paper, or Scissors");
-let computerSelection = getComputerChoice();
+    let playerScore = 0;
+    let computerScore = 0;
+    let continueGame = true;
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+
+    playerScore = 0;
+    computerScore = 0;
+    
+
+    console.log("First to 5 Rock, Paper, Scissors Game")
+
+    while (continueGame) {
+
+        console.log("Your Score = " + playerScore);
+        console.log("Computer's Score = " + computerScore);
+
+        let playerSelection = prompt("Select Rock, Paper, or Scissors");
+        let computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(computerScore);
+
+        if (playerScore == 5) {
+            continueGame = false;
+            console.log("Your Score = " + playerScore);
+            console.log("Computer's Score = " + computerScore);
+            console.log("Congrats! You have won the game and beaten the computer!");
+        } else if (computerScore == 5) {
+            continueGame = false;
+            console.log("Your Score = " + playerScore);
+            console.log("Computer's Score = " + computerScore);
+            console.log("Dang it! The computer won!");
+        }
+
+    }
+
+}
+
+game();
+
+
+
+
 
 
